d0691869d1abae7309c9d29002149c2f18ed4558
sujeet kumar <sujeetkr503@gmail.com>
2024-10-07 17:55:46 +0530

    fully functional

--- Drum Kit/app.js
+++ Drum Kit/app.js
@@ -14,17 +14,29 @@ const sounds = {
 drumPads.forEach(pad => {
     pad.addEventListener('click', () => {
         playSound(pad.getAttribute('data-key'));
+        
     });
 });
 
 document.addEventListener('keydown', event => {
-    const key = event.keyCode;
+    const key = event.keyCode.toString();
+    // console.log(key);
     if (sounds[key]) {
         playSound(key);
+        for (let i = 0; i < drumPads.length; i++) {
+            if (drumPads[i].getAttribute('data-key') === key) {
+                drumPads[i].classList.add('playing');
+                setTimeout(() => {
+                    drumPads[i].classList.remove('playing');
+                }, 200);
+            }
+        }
     }
+
 });
 
 function playSound(key) {
     const audio = new Audio(`sounds/${sounds[key]}.wav`);
     audio.play();
+   
 }
