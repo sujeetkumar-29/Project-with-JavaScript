b69d8d1d6a18f1f2c85c949a243dd331b10bed6e
sujeet kumar <sujeetkr503@gmail.com>
2024-10-09 20:37:47 +0530

    password

---
+++ random number generator/app.js
@@ -0,0 +1,15 @@
+function generatePassword(length) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=";
+    let password = "";
+  
+    for (let i = 0; i < length; i++) {
+      password =password+ characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+  
+    return password;
+  }
+  
+  // Example usage:
+  const password = generatePassword(8);
+  
+  console.log(password);
