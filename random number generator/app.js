7bad90176bde9f580df4980c60689f39236fb5c9
sujeet kumar <sujeetkr503@gmail.com>
2024-10-10 19:12:25 +0530

    progress

--- random number generator/app.js
+++ random number generator/app.js
@@ -1,8 +1,8 @@
 let pass=document.querySelector("#password");
-let withLowerCase=document.querySelector(".lowerCase").checked;
-let withUpperCase=document.querySelector(".upperCase").checked;
-let withSpecialChar=document.querySelector(".specialChar").checked;
-let withNumbers=document.querySelector(".numbers").checked;
+let withLowerCase=document.querySelector(".lowerCase");
+let withUpperCase=document.querySelector(".upperCase");
+let withSpecialChar=document.querySelector(".specialChar");
+let withNumbers=document.querySelector(".numbers");
 let genPass=document.querySelector(".genPass");
 
 let length=document.querySelector(".length").value;
@@ -13,29 +13,26 @@ function generatePassword(length) {
     const lowerCase="abcdefghijklmnopqrstuvwxyz";
     const numbers = "0123456789";
     const specialChar="!@#$%^&*()_+-=";
+    let char="";
+     if(withUpperCase.checked){
+        char+=upperCase;
+       
+    }
+     if(withLowerCase.checked){
+        char+=lowerCase;
+    }
+    
+    if(withSpecialChar.checked){
+        char+=specialChar;
+    }
+    if(withNumbers.checked){
+        char+=numbers;
+    }
+    
     let password = "";
-    if(withUpperCase){
-
-        for (let i = 0; i < length; i++) {
-            password = password + upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+     for (let i = 0; i < length; i++) {
+            password +=   char.charAt(Math.floor(Math.random() * char.length));
         }
-    }
-    if(withLowerCase){
-         for (let i = 0; i < length; i++) {
-        password = password + lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
-    }
-    }
-   if(withSpecialChar){
-    for (let i = 0; i < length; i++) {
-        password = password + specialChar.charAt(Math.floor(Math.random() * specialChar.length));
-    }
-   }
-    if(withNumbers){
-         for (let i = 0; i < length; i++) {
-        password = password + numbers.charAt(Math.floor(Math.random() * numbers.length));
-    }
-
-    }
    
     return password;
 }
