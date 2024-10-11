60af4dfe0e7f18f2a8edd80e1dac3ecd6b1d9a32
sujeet kumar <sujeetkr503@gmail.com>
2024-10-11 16:08:07 +0530

    js done

--- random number generator/app.js
+++ random number generator/app.js
@@ -1,47 +1,44 @@
-let pass=document.querySelector("#password");
-let withLowerCase=document.querySelector(".lowerCase");
-let withUpperCase=document.querySelector(".upperCase");
-let withSpecialChar=document.querySelector(".specialChar");
-let withNumbers=document.querySelector(".numbers");
-let genPass=document.querySelector(".genPass");
+let pass = document.querySelector("#password");
+let withLowerCase = document.querySelector(".lowerCase");
+let withUpperCase = document.querySelector(".upperCase");
+let withSpecialChar = document.querySelector(".specialChar");
+let withNumbers = document.querySelector(".numbers");
+let genPass = document.querySelector(".genPass");
 
-let length=document.querySelector(".length").value;
 // let length=" ";
-function generatePassword(length) {
-  
+function generatePassword() {
+    let length = document.querySelector(".length").value;
+
+
     const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowerCase="abcdefghijklmnopqrstuvwxyz";
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
     const numbers = "0123456789";
-    const specialChar="!@#$%^&*()_+-=";
-    let char="";
-     if(withUpperCase.checked){
-        char+=upperCase;
-       
+    const specialChar = "!@#$%^&*()_+-=";
+    let char = "";
+    if (withUpperCase.checked) {
+        char += upperCase;
     }
-     if(withLowerCase.checked){
-        char+=lowerCase;
+    if (withLowerCase.checked) {
+        char += lowerCase;
     }
-    
-    if(withSpecialChar.checked){
-        char+=specialChar;
+    if (withSpecialChar.checked) {
+        char += specialChar;
     }
-    if(withNumbers.checked){
-        char+=numbers;
+    if (withNumbers.checked) {
+        char += numbers;
     }
-    
+
     let password = "";
-     for (let i = 0; i < length; i++) {
-            password +=   char.charAt(Math.floor(Math.random() * char.length));
-        }
-   
+    for (let i = 0; i < length; i++) {
+        password += char.charAt(Math.floor(Math.random() * char.length));
+    }
+
     return password;
 }
-// console.log(length);
-// Example usage:
-genPass.addEventListener("click",()=>{
-   
-    const password = generatePassword(length);
-    console.log("generated password clicked!");
-    pass.value=password;
+
+genPass.addEventListener("click", () => {
+
+    const password = generatePassword();
+    // console.log("generated password clicked!");
+    pass.value = password;
 })
-// console.log(password);
