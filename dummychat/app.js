b16c77c14e3cb900cb6dafc7f70923e98dd48b0d
sujeet kumar <sujeetkr503@gmail.com>
2024-11-26 07:36:56 +0530

    progress

--- dummychat/app.js
+++ dummychat/app.js
@@ -1,12 +1,23 @@
 let sender=document.querySelector(".sender");
 let input=document.querySelector("input");
+let chats=document.querySelector(".chats");
 
 let button=document.querySelector("button");
 
 button.addEventListener("click",()=>{
-    let text=input.value;
-    console.log("button clicked!");
-    console.log(text);
+    let p=document.createElement("p");
+    p.innerHTML=input.value;
+    p.classList.add("sender");
+    
+    
+   chats.appendChild(p);
     input.value="";
+    scrollToBottom();
 })
+
+
+function scrollToBottom() {
+    // const chatsContainer = document.getElementById('chats');
+    chats.scrollTop = chats.scrollHeight;
+}
 
