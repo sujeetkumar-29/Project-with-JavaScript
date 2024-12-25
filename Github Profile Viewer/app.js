185ced75c1897aaf04e600c7dde00fb5969f5bf0
sujeet kumar <sujeetkr503@gmail.com>
2024-12-25 17:08:16 +0530

    github profile viewer progress

--- Github Profile Viewer/app.js
+++ Github Profile Viewer/app.js
@@ -1,10 +1,45 @@
+const name=document.querySelector('#name');
+const bio=document.querySelector('#bio');
+const followers=document.querySelector('#followers');
+const following=document.querySelector('#following');
+const repos=document.querySelector('#repos');
+const avatar=document.querySelector('#avatar');
+const loginDate=document.querySelector('#loginDate');
+const lastOnline=document.querySelector('#lastOnline');
+const profileLink=document.querySelector('#profileLink');
+
 
 function findUser(){
-    let username = document.querySelector('#username').value;
-    console.log(username);
+    let usernameInput = document.querySelector('#username');
+    let username = usernameInput.value;
+    if(username === ''){
+        alert('Please enter a username');
+        return;
+    }
     fetch(`https://api.github.com/users/${username}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        if(data.message === 'Not Found'){
+            alert('User not found');
+            return;
+        }
+        console.log(data);
+        usernameInput.value= '';
+        avatar.src = data.avatar_url;
+        name.innerHTML = data.name;
+        bio.innerHTML=data.bio;
+        followers.innerHTML = data.followers;
+        following.innerHTML = data.following;
+        repos.innerHTML = data.public_repos;
+        loginDate.innerHTML = new Date(data.created_at).toDateString();
+        lastOnline.innerHTML =new Date(data.updated_at).toDateString(); 
+        profileLink.href = data.html_url;
+    
+    })
     .catch(error => console.log(error));
+
+
+    
 }
+
 
